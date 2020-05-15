0bace49b688f6158490ff93804ff94caa6203f02
Aleksandar Trujic <aleksandar.trujic@live.com>
2020-05-15 12:25:09 -0500

    manifest name and logo changed

--- gatsby-config.js
+++ gatsby-config.js
@@ -29,13 +29,13 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
+        name: `AleksandarTrujic`,
+        short_name: `AT`,
         start_url: `/`,
         background_color: `#121212`,
         theme_color: `#121212`,
         display: `minimal-ui`,
-        icon: `src/images/logo-dark.png`, // This path is relative to the root of the site.
+        icon: `src/images/logo.png`, // This path is relative to the root of the site.
       },
     },
     `gatsby-plugin-offline`,
